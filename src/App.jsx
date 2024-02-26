4466c33ba0f70de5ffeec217045bc4f171bbd041
BRP7 <bhumiparmar0007@gmail.com>
2024-02-26 10:23:28 +0530

    Basic React Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  function increFun(){
+    return setCount(count+1);//u can also write callback inside setFun & access of prev value as paramater of that callback
+  }
+  function decreFun(){
+    return setCount(count-1);//u can also write callback inside setFun & access of prev value as paramater of that callback
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Header : {count}</h1>
+      <h2>Counter Increment & Decrement </h2>
+      <button onClick={increFun}>Increment : {count}</button>
+      {" "}
+      <button onClick={decreFun}>Decrement : {count} </button>
+      <h1>Footer : {count}</h1>
     </>
   )
 }
